bf0395b383da55828b53a46dfc3fc39752b0b89d
calm-rad <59620121+calm-rad@users.noreply.github.com>
2020-01-10 11:58:40 -0500

    TEMP - Remove sodium-native module in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-try {
-  if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
-  module.exports = require('sodium-native')
-} catch (err) {
-  module.exports = require('sodium-javascript')
-}
+// TEMP - Remove sodium-native until you can get nodejs-mobile to compile with it
+// try {
+//   if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
+//   module.exports = require('sodium-native')
+// } catch (err) {
+//   module.exports = require('sodium-javascript')
+// }
+module.exports = require('sodium-javascript')

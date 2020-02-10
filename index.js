a4da8444639bb303620feb4175edde95fded48b8
Calm Rad <calm_rad@protonmail.com>
2020-02-09 22:43:27 -0500

    Use sodium-native-nodejs-mobile

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// TEMP - Remove sodium-native until you can get nodejs-mobile to compile with it
-// try {
-//   if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
-//   module.exports = require('sodium-native')
-// } catch (err) {
-//   module.exports = require('sodium-javascript')
-// }
-module.exports = require('sodium-javascript')
+try {
+  if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
+  module.exports = require('sodium-native-nodejs-mobile')
+} catch (err) {
+  module.exports = require('sodium-javascript')
+}
